482b0de239bc2d8c651e73d6ebbeb3ef4e7f15a3
Victor Miletsky <victor.miletskiy@gmail.com>
2018-09-07 21:23:38 +0300

    Generate custom lodash build

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,40 @@
 var gulp = require('gulp'),
-  jshint = require('gulp-jshint'),
   rename = require('gulp-rename'),
   uglify = require('gulp-uglify'),
-  bower = require('gulp-bower');
-  concat = require('gulp-concat');
-  customLodash = require('gulp-lodash-custom');
+  bower = require('gulp-bower'),
+  concat = require('gulp-concat'),
+  insert = require('gulp-insert'),
+  customLodash = require('gulp-lodash-custom')
+;
 
 gulp.task('bower', function() {
   return bower();
 });
 
-gulp.task('app-build', ['bower'], function() {
-  gulp.src('opentok-angular.js')
-    .pipe(jshint())
+gulp.task('prepare-opentok', /*['bower'],*/ function() {
+  return gulp.src('src/opentok-angular.js')
+    .pipe(insert.prepend('; console.log(lodash); var _ = lodash; '))
+    .pipe(gulp.dest('./.tmp'));
+});
+
+gulp.task('prepare-lodash', function () {
+  const lodashBin = require('lodash-cli/package.json').bin.lodash;
+  const lodashCli = require.resolve('lodash-cli/' + lodashBin);
+
+  return gulp.src('opentok-angular.js')
+    .pipe(customLodash({
+      lodashCli: lodashCli + ' exports=global iife="%output%" ',
+      target: ' ./.tmp/lodash-custom.min.js'
+    }));
+});
+
+gulp.task('default', ['prepare-opentok', 'prepare-lodash'], function(){
+  gulp.src(['.tmp/lodash-custom.min.js', '.tmp/opentok-angular.js'])
+    .pipe(concat('opentok-angular.js'))
+    .pipe(gulp.dest('./'))
     .pipe(uglify({
       preserveComments: 'some'
     }))
     .pipe(rename('opentok-angular.min.js'))
-    .pipe(gulp.dest('./'));
-});
-
-gulp.task('add-lodash', function () {
-  gulp.src('opentok-angular.js')
-      .pipe(customLodash({}))
-      .pipe(gulp.dest('./'));
-});
-
-gulp.task('default', ['add-lodash', 'app-build'], function(){
-  gulp.src(['lodash-custom.min.js', 'opentok-angular.min.js'])
-    .pipe(concat('build.js'))
     .pipe(gulp.dest('./'));
 });
